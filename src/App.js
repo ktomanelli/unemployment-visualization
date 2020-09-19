a96b2a4650f84f982361ab5a2063082dbd4e730b
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-09-19 13:42:17 -0400

    added csv file + csv parse function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import Papa from 'papaparse'
+const pathToCsv=process.env.PUBLIC_URL+'/employment-by-state.csv'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+
+  useEffect(()=>{
+    Papa.parse(pathToCsv,{
+      header:true,
+      download: true,
+      skipEmptyLines: true,
+      complete: (result)=>console.log(result.data)
+    })
+  },[])
+
+  return(
+    <h1>Hello World</h1>  
+  )
 }
 
 export default App;
