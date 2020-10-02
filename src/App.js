ab280b68fef34927f05f159aba459164e5ac6312
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-10-02 18:10:34 -0400

    added populations.json and changed visualizationto display change per 1000 people

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,useState} from 'react';
 import Papa from 'papaparse'
 import Mapbox from './Mapbox'
 const pathToCsv=process.env.PUBLIC_URL+'/employment-by-state.csv'
 
 const App = ()=>{
 
+  const [data,setData] = useState(null)
   useEffect(()=>{
     Papa.parse(pathToCsv,{
       header:true,
       download: true,
       skipEmptyLines: true,
-      complete: (result)=>console.log(result.data)
+      complete: (result)=>setData(result.data)
     })
   },[])
 
   return(
     <>
-    <h1>Hello World</h1>  
-    <Mapbox/>
+    <h1>Hello World</h1> 
+    {data?<Mapbox data={data}/>:''}
     </>
   )
 }
--- src/Mapbox.js
+++ src/Mapbox.js
@@ -1,6 +1,7 @@
 import React, {useState,useEffect,useRef} from 'react'
 import mapboxgl from 'mapbox-gl'
 import states from './us-states.json'
+import population from './us-populations.json'
 
 import "mapbox-gl/dist/mapbox-gl.css";
 require('dotenv').config()
@@ -13,22 +14,77 @@ const styles = {
 
 const Mapbox = (props) => {
     const mapContainer = useRef(null)
-    const [map, setMap] = useState(null);
-    
-    useEffect(()=>{
-        mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
-        const initializeMap=({setMap,mapContainer})=>{
-            const map = new mapboxgl.Map(
-                {
-                    container: mapContainer.current,
-                    style: 'mapbox://styles/mapbox/streets-v11',
-                    center: [-74, 40.72],
-                    zoom: 10.3
-                }
-            )
+
+    const r = 152
+    const g = 251
+    const b = 152
+
+    function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+    }
+      
+    function rgbToHex(r, g, b) {
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    }
+    const getColors=(max)=>{
+        const arr=[]
+        for(let i=0;i<=max;i+=7){
+            arr.push([i,rgbToHex(parseInt(i/7 * r/10), parseInt(i/7 * g/10), parseInt(i/7 * b/10))])
         }
-        if(!map) initializeMap({setMap,mapContainer})
-    }, [map])
+        console.log(arr)
+        return arr
+    }
+
+
+
+    useEffect(()=>{
+        
+        addDataToSource()
+
+        mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
+        const map = new mapboxgl.Map(
+            {
+                container: mapContainer.current,
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [-95, 40.72],
+                zoom: 3
+            }
+        )
+        map.on("load", () => {
+            map.resize();
+            map.addSource(`states`,{
+                type:'geojson',
+                data:states
+            })
+            map.addLayer({
+                id:`states-fill`,
+                'type': 'fill',
+                'source': `states`,
+                'paint': {
+                    'fill-color': {
+                        property: 'change',
+                        stops: getColors(76)
+                    },
+                    'fill-opacity': 0.6
+                }
+            })
+        });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+
+    const addDataToSource= () => states.features.forEach(state=>{
+            const name = state.properties.name
+            const stateData = props.data.find(item=>(item.State===name))
+            if(stateData){
+                let changeInt = parseInt(stateData.Change)
+                if(changeInt<0) changeInt*=-1
+                state.properties.change = parseInt(changeInt/(population[name]/1000))
+                console.log(state.properties.change)
+            }
+        })
+    
 
     return(
         <div ref={el=>mapContainer.current=el} style={styles} />
