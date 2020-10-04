100d38c658fe95aa27164fbf147123c075fb11a4
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-10-04 14:25:10 -0400

    added interactivity to map so unemployment data displays on state mouse-over

--- src/Mapbox.js
+++ src/Mapbox.js
@@ -9,10 +9,26 @@ require('dotenv').config()
 const styles = {
     width: "100vw",
     height: "calc(100vh - 80px)",
-    position: "absolute"
+    position: "relative"
   };
+const dataStyles={
+    position: 'absolute',
+    zIndex:'1',
+    bottom: '',
+    right: '0',
+    width:'30%',
+    display:'block',
+    color:'white',
+    backgroundColor:'#282a36',
+}
 
 const Mapbox = (props) => {
+    const [mouseOverState,setMouseOverState] = useState({
+        name:'',
+        population:'',
+        change:'',
+        changePerPop:'',
+    })
     const mapContainer = useRef(null)
 
     const r = 152
@@ -63,13 +79,24 @@ const Mapbox = (props) => {
                 'source': `states`,
                 'paint': {
                     'fill-color': {
-                        property: 'change',
+                        property: 'changePerPop',
                         stops: getColors(76)
                     },
                     'fill-opacity': 0.6
                 }
             })
         });
+
+        map.on('mousemove','states-fill',e=>{
+            map.getCanvas().style.cursor = 'pointer';
+            const name = e.features[0].properties.name
+            setMouseOverState({
+                name,
+                population:population[name],
+                change:e.features[0].properties.change,
+                changePerPop:e.features[0].properties.changePerPop,
+            })  
+        })
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
@@ -78,16 +105,24 @@ const Mapbox = (props) => {
             const name = state.properties.name
             const stateData = props.data.find(item=>(item.State===name))
             if(stateData){
+                state.properties.change = stateData.Change
                 let changeInt = parseInt(stateData.Change)
                 if(changeInt<0) changeInt*=-1
-                state.properties.change = parseInt(changeInt/(population[name]/1000))
-                console.log(state.properties.change)
+                state.properties.changePerPop = parseInt(changeInt/(population[name]/1000))
             }
         })
     
 
     return(
-        <div ref={el=>mapContainer.current=el} style={styles} />
+        <div>
+            <div className='stateData' style={dataStyles}>
+                <h4>{mouseOverState.name}</h4>
+                <p>State Population:<br/>{mouseOverState.population}</p>
+                <p>Change in State Unemployment:<br/>{mouseOverState.change}</p>  
+                <p>Change in State Unemployment Per 1000 People:<br/>{mouseOverState.changePerPop*-1}</p>  
+            </div>
+            <div ref={el=>mapContainer.current=el} style={styles} />
+        </div>
     )
 }
 
